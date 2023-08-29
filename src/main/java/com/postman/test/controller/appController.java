6e3e0d2bc127cc4c81405710af2b8acb9cc920d4
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-08-29 17:51:40 -0300

    I practiced ResponseBody and ResponseEntity

--- src/main/java/com/postman/test/controller/appController.java
+++ src/main/java/com/postman/test/controller/appController.java
@@ -1,9 +1,14 @@
 package com.postman.test.controller;
 
 import com.postman.test.Cliente;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,8 +24,24 @@ public class appController {
         
         System.out.println("Cliente creado");
         System.out.println("Nombre: " + cli.getNombre());
-        System.out.println("Apellido: " + cli.getApellido());
-        
-        
+        System.out.println("Apellido: " + cli.getApellido());    
     }
+    
+    @GetMapping("/cliente/traer")
+    @ResponseBody
+    public List<Cliente> traerClientes(){
+        
+        List<Cliente> clientList = new ArrayList<>();
+        clientList.add(new Cliente(1l, "Leo", "Messi"));
+        clientList.add(new Cliente(2l, "Cristiano", "Ronaldo"));
+        clientList.add(new Cliente(3l, "Neymar", "Santos"));
+        
+        return clientList ;
+    }
+    
+    @GetMapping("/customresponse")
+    public ResponseEntity<String> custom() {
+        return new ResponseEntity<>("Request exitosa!!!", HttpStatus.NOT_FOUND);
+    }
+    
 }
